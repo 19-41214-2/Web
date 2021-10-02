a4a96ba517642888f8e9dd33410b38f7c55ce91c
Tuhin <mdtuhin.md19@gmail.com>
2021-10-02 15:17:19 +0600

    abc

---
+++ task2.php
@@ -0,0 +1,69 @@
+
+<html>
+	<head></head>
+	<body>
+<h1><b>Registration Form</b></h1>
+		<fieldset>
+		<legend><b>Basic Information</b></legend>
+
+		<form action="" method="post">
+			<table>
+				<tr>
+					<td><span><b>Enter your name</b>:</span></td>
+					<td>:<input type="text" name="name" ><br>
+					
+				</tr>
+				<tr>
+					<td><span><b>Gender</b>:</span></td>
+					<td>:<input type="radio" name="gender"<span>Male</span>
+					    <input type="radio" name="gender" <span>Female</span></td>
+					
+				</tr>
+			</table>
+			
+			
+			
+		
+		</fieldset>
+		<fieldset>
+		<legend><b>Contract Information</b></legend>
+
+		<form action="" method="post">
+			<table>
+				<tr>
+					<td><span><b>Email</b>:</span></td>
+					<td>:<input type="text" name="email" ><br>
+					
+				</tr>
+				<tr>
+					<td><span><b>Phone</b>:</span></td>
+					<td>:<input type="number_format" name="phone" ><br>
+					
+				</tr>
+			</table>
+		</form>
+		</fieldset>
+		
+		
+		
+		</fieldset>
+		<fieldset>
+		<legend><b>Academic Information</b></legend>
+
+		<form action="" method="post">
+			<table>
+				<tr>
+					<td><span><b>Info</b>:</span></td>
+					<td>:<input type="text" name="info" ><br>
+					
+				</tr>
+			</table>		
+		</form>
+		</fieldset>
+		
+		
+		
+		
+		<center><input type="submit" name="Register"></center>
+	</body>
+</html> 
