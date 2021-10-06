f0b8f12aa11d66047a9011de56544b3658fb2b23
Md Arifur Rahman Tuhin <91674000+19-41214-2@users.noreply.github.com>
2021-10-06 11:07:22 +0600

    Add files via upload

---
+++ form submission .php
@@ -0,0 +1,100 @@
+
+<html>
+	<head></head>
+	<body>
+<h1><b>Registration Form</b></h1>
+		<fieldset>
+		<legend><b>Basic Information</b></legend>
+
+		<form action="" method="post">
+			<table>
+				<tr>
+					<td><span><b>First name</b>:</span></td>
+					<td>:<input type="text" name="name" ><br>
+					
+				</tr>
+				<tr>
+					<td><span><b>Last name</b>:</span></td>
+					<td>:<input type="text" name="name" ><br>
+					
+				</tr>
+				<tr>
+					<td><span><b>Gender</b>:</span></td>
+					<td>:<input type="radio" name="gender"<span>Male</span>
+					    <input type="radio" name="gender" <span>Female</span></td>
+					
+				</tr>
+				<tr>
+					<td><span><b>Date</b></span></td>
+					<td>:<input type="date" name="date"></td>
+				</tr>
+			</table>
+			
+			
+			
+		
+		</fieldset>
+		<fieldset>
+		<legend><b>Contract Information</b></legend>
+
+		
+			<table>
+				<tr>
+					<td><span><b>Present address</b>:</span></td>
+					<td>:<input type="text" name="email" ><br>
+					
+				</tr>
+				<tr>
+					<td><span><b>Permanent address</b>:</span></td>
+					<td>:<input type="text" name="email" ><br>
+					
+				</tr>
+				<tr>
+					<td><span><b>Email</b>:</span></td>
+					<td>:<input type="text" name="email" ><br>
+					
+				</tr>
+				<tr>
+					<td><span><b>Phone</b>:</span></td>
+					<td>:<input type="number_format" name="phone" ><br>
+					
+				</tr>
+				<tr>
+					<td><span><b>Personal Website Link</b>:</span></td>
+					<td>:<input type="link" name="personal website link" ><br>
+					
+				</tr>
+			</table>
+		
+		</fieldset>
+		
+		
+		
+		
+		<fieldset>
+		<legend><b>Academic Information</b></legend>
+		
+
+		
+			<table>
+				<tr>
+					<td><span><b>Username</b>:</span></td>
+					<td>:<input type="text" name="info" ><br>
+					
+				</tr>
+				<table>
+				<tr>
+					<td><span><b>Password</b>:</span></td>
+					<td>:<input type="text" name="password" ><br>
+					
+				</tr>
+			</table>			
+		</fieldset>
+		
+		
+		
+		
+		<center><input type="submit" name="Register"></center>
+		</form>
+	</body>
+</html> 
