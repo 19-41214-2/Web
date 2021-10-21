781b51c27383e26a30799c82dbfdece7b9677034
Md Arifur Rahman Tuhin <91674000+19-41214-2@users.noreply.github.com>
2021-10-21 10:27:57 +0600

    Add files via upload

---
+++ add_bene.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Page2 [Add Beneficiary]</title>
+</head>
+<body>
+<h1>Page2 [Add Beneficiary]</h1>
+<b><p>Digital Wallet</p></b>
+<a href="http://localhost/Work/MidTask/home.php">1. Home</a>
+<a href="http://localhost/Work/MidTask/add_bene.php">2. Add Beneficiary</a>
+<a href="http://localhost/Work/MidTask/tran_history.php">3. Add Beneficiary</a><br>
+
+<label for="amount">Add beneficially:</label>
+<input type="number" id="amount" name="amount"><br>
+<label for="amount">Mobile no:</label>
+<input type="number" id="amount" name="amount"><br>
+</body>
+</html>
