1747bffcfbffbd1f6705f0ae9e159958274459da
Md Arifur Rahman Tuhin <91674000+19-41214-2@users.noreply.github.com>
2021-10-21 10:59:11 +0600

    Add files via upload

---
+++ tran_history.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Page3 [Transaction History]</title>
+</head>
+<body>
+<h1>Page3 [Transaction History]</h1>
+<b><p>Digital Wallet</p></b>
+<a href="http://localhost/Work/MidTask/home.php">1. Home</a>
+<a href="http://localhost/Work/MidTask/add_bene.php">2. Add Beneficiary</a>
+<a href="http://localhost/Work/MidTask/tran_history.php">3. Add Beneficiary</a><br>
+
+<label for="amount">From:</label>
+<input type="number" id="amount" name="amount"><br>
+<label for="amount">To:</label>
+<input type="number" id="amount" name="amount"><br>
+
+</body>
+</html>
