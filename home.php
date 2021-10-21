be62f022e1f88d8836fb5a76ecca272c653f241a
Md Arifur Rahman Tuhin <91674000+19-41214-2@users.noreply.github.com>
2021-10-21 10:28:52 +0600

    Add files via upload

---
+++ home.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Page 1 (Home)</title>
+</head>
+<body>
+<h1>Page1 [Home]</h1>
+<b><p>Digital Wallet</p></b>
+<a href="http://localhost/Work/MidTask/home.php">1. Home</a>
+<a href="http://localhost/Work/MidTask/add_bene.php">2. Add Beneficiary</a>
+<a href="http://localhost/Work/MidTask/tran_history.php">3. Add Beneficiary</a>
+
+<b><p>Fund Transfer:</p></b>
+
+<label for="Transfer">Select category:</label>
+<select  name="Transfer" id="Transfer">
+    <option value="Transfer">Transfer</option>
+    <option value="Add">Add money</option>
+</select><br>
+
+<label for="Transfer">To:</label>
+<select  name="Transfer" id="Transfer">
+    <option value="Rahim">Rahim</option>
+    <option value="Karim">Karim</option>
+</select><br>
+<label for="amount">Amount:</label>
+<input type="number" id="amount" name="amount"><br>
+<input type="submit" name="Submit">
+
+</body>
+
+
